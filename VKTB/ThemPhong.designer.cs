fbde35ef3a82ac5c78b0f3b75f866989cb63e9cf
Tung Lam <lam312abc@gmail.com>
2023-01-07 20:02:24 +0700

    NhapTB QLPTN-29

---
+++ VKTB/ThemPhong.designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace VKTB
+{
+    partial class ThemPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMaPhong = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnQuayLai = new DevExpress.XtraEditors.SimpleButton();
+            this.btnThem = new DevExpress.XtraEditors.SimpleButton();
+            this.cmbBoMon = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDiaDiem = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMaPhong
+            // 
+            this.txtMaPhong.Location = new System.Drawing.Point(274, 55);
+            this.txtMaPhong.Name = "txtMaPhong";
+            this.txtMaPhong.Size = new System.Drawing.Size(185, 23);
+            this.txtMaPhong.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Mã Phòng";
+            // 
+            // btnQuayLai
+            // 
+            this.btnQuayLai.Location = new System.Drawing.Point(331, 297);
+            this.btnQuayLai.Name = "btnQuayLai";
+            this.btnQuayLai.Size = new System.Drawing.Size(149, 45);
+            this.btnQuayLai.TabIndex = 17;
+            this.btnQuayLai.Text = "Quay Lại";
+            this.btnQuayLai.Click += new System.EventHandler(this.btnQuayLai_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(102, 293);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(173, 49);
+            this.btnThem.TabIndex = 16;
+            this.btnThem.Text = "Thêm phòng";
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // cmbBoMon
+            // 
+            this.cmbBoMon.FormattingEnabled = true;
+            this.cmbBoMon.Location = new System.Drawing.Point(274, 173);
+            this.cmbBoMon.Name = "cmbBoMon";
+            this.cmbBoMon.Size = new System.Drawing.Size(185, 24);
+            this.cmbBoMon.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(129, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Bộ môn";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(274, 93);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(185, 23);
+            this.txtTen.TabIndex = 20;
+            this.txtTen.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Tên phòng";
+            // 
+            // txtDiaDiem
+            // 
+            this.txtDiaDiem.Location = new System.Drawing.Point(274, 131);
+            this.txtDiaDiem.Name = "txtDiaDiem";
+            this.txtDiaDiem.Size = new System.Drawing.Size(185, 23);
+            this.txtDiaDiem.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(129, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Địa điểm";
+            // 
+            // ThemPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 471);
+            this.Controls.Add(this.txtDiaDiem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnQuayLai);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMaPhong);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBoMon);
+            this.Name = "ThemPhong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NhapVuKhi";
+            this.Load += new System.EventHandler(this.ThemPhong_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtMaPhong;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton btnQuayLai;
+        private DevExpress.XtraEditors.SimpleButton btnThem;
+        private System.Windows.Forms.ComboBox cmbBoMon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDiaDiem;
+        private System.Windows.Forms.Label label3;
+    }
+}
